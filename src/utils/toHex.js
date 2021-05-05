813f24e408ce45e1c4aea88d8841feb46f272a12
Ivan Malkov <ozonep@yandex.ru>
2021-05-05 17:01:06 +0300

    refactor: 💡 Removed files I don't need anymore

--- src/utils/toHex.js
+++
@@ -1,5 +0,0 @@
-export function toHex(buffer) {
-  const hashArray = Array.from(new Uint8Array(buffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-  return hashHex
-}
