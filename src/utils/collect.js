00215c38e4878bf4a846e93174779cd2e1ef7b88
Ivan Malkov <ozonep@yandex.ru>
2021-05-05 20:28:55 +0300

    fix: 🐛 Had to switch back to old implementation

--- src/utils/collect.js
+++ src/utils/collect.js
@@ -1,10 +1,12 @@
+import { forAwait } from './forAwait.js'
+
 export async function collect(iterable) {
   let size = 0
   const buffers = []
-  for await (const val of iterable) {
-    buffers.push(val)
-    size += val.byteLength
-  }
+  await forAwait(iterable, value => {
+    buffers.push(value)
+    size += value.byteLength
+  })
   const result = new Uint8Array(size)
   let nextIndex = 0
   for (const buffer of buffers) {
